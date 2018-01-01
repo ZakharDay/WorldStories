a111f7449f895cf33259c2bc67a3e6b969070845
ZakharDay <zakhar.day@gmail.com>
2018-01-01 12:25:49 +0800

    URL moved

--- src/Stories.js
+++ src/Stories.js
@@ -4,7 +4,7 @@ class Stories extends React.Component {
 
   constructor(props) {
     super(props)
-    
+    const authURL = "https://www.instagram.com/oauth/authorize/?client_id=79a9886651e94b4081decc663f1e91fd&redirect_uri=http://hack.exchange&response_type=code"
   }
 
 
