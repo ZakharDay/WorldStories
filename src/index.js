1d4964084f3c175a3e595d66cace849209ea5499
ZakharDay <zakhar.day@gmail.com>
2018-01-01 12:24:58 +0800

    ...

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
+import React from 'react'
+import { render } from 'react-dom'
+import Hello from './Hello'
 
 const styles = {
   fontFamily: 'sans-serif',
   textAlign: 'center',
-};
+}
 
 const authURL = "https://www.instagram.com/oauth/authorize/?client_id=79a9886651e94b4081decc663f1e91fd&redirect_uri=http://hack.exchange&response_type=code"
+
+
 
 const App = () => (
   <div style={styles}>
     <Hello name="CodeSandbox" />
     <h2>Start editing to see some magic happen {'\u2728'}</h2>
   </div>
-);
+)
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'))
