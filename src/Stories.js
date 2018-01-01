dcefcbb049ff586f6b9ba8361b7d3c584d815c0c
ZakharDay <zakhar.day@gmail.com>
2018-01-01 12:30:16 +0800

    Client ID moved to const

--- src/Stories.js
+++ src/Stories.js
@@ -5,7 +5,8 @@ class Stories extends React.Component {
   constructor(props) {
     super(props)
     const authURL = "https://www.instagram.com/oauth/authorize/?client_id=79a9886651e94b4081decc663f1e91fd&redirect_uri=http://hack.exchange&response_type=code"
-  }
+    const clientID = "79a9886651e94b4081decc663f1e91fd"
+   }
 
 
 
