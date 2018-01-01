52cd645908421afd00222932cc4ed76411ba4ac5
ZakharDay <zakhar.day@gmail.com>
2018-01-01 12:13:32 +0800

    Instagram auth URL added

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const styles = {
   textAlign: 'center',
 };
 
-const authURL = ""
+const authURL = "https://www.instagram.com/oauth/authorize/?client_id=79a9886651e94b4081decc663f1e91fd&redirect_uri=http://hack.exchange&response_type=code"
 
 const App = () => (
   <div style={styles}>
