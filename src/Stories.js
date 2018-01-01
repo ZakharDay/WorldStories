8901b2e049c5e6e01fca385f55acbf67cd482cf6
ZakharDay <zakhar.day@gmail.com>
2018-01-01 12:48:20 +0800

    Auth call added

--- src/Stories.js
+++ src/Stories.js
@@ -6,8 +6,27 @@ class Stories extends React.Component {
     super(props)
     const authURL = "https://www.instagram.com/oauth/authorize/?client_id=79a9886651e94b4081decc663f1e91fd&redirect_uri=http://hack.exchange&response_type=code"
     const clientID = "79a9886651e94b4081decc663f1e91fd"
-   }
-
-
+    this.props = {
+      authURL: authURL,
+      clientID: clientID
+    }
+    this.state = {token: ""}
+  }
+  
+  componentDidMount() {
+    this.state = {token: this.httpGet(this.props.authURL)}
+  }
+  
+  httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+  }
+  
+  render() {
+    return(
+      <div>{this.state.token}</div>
+    ) 
 
 }
